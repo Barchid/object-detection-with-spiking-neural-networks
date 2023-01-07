1821b6ba277e2db453d36b64ac998279c4d80998
Sami Barchid <sami.barchid@univ-lille.fr>
2023-01-08 00:00:36 +0100

    fix cupy backend

--- models/utils.py
+++ models/utils.py
@@ -8,7 +8,7 @@ from .spiking_vgg import *
 
 class SpikingBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, 
-                   bias=False, stride=1, padding=0, groups=1, backend='cupy'):
+                   bias=False, stride=1, padding=0, groups=1, backend='torch'):
         super().__init__()
         
         self.bn_conv = layer.SeqToANNContainer(
@@ -42,14 +42,14 @@ def get_model(args):
             2*args.tbin, norm_layer=norm_layer,
             multi_step_neuron=ms_neuron,
             growth_rate=int(growth_rate), block_config=blocks[depth],
-            num_classes=2, backend="cupy",
+            num_classes=2, backend="torch",
         )
     elif family == "mobilenet":
         return multi_step_spiking_mobilenet(
             2*args.tbin, norm_layer=norm_layer,
             in_channels=int(version),
             multi_step_neuron=ms_neuron,
-            num_classes=2, dw=True, backend="cupy", 
+            num_classes=2, dw=True, backend="torch", 
         )
     elif family == "squeezenet":
         squeezenets = {
@@ -59,14 +59,14 @@ def get_model(args):
         return squeezenets[version](
             2*args.tbin, norm_layer=norm_layer,
             multi_step_neuron=ms_neuron,
-            num_classes=2, backend="cupy"
+            num_classes=2, backend="torch"
         )
     elif family == "vgg":
         if version == "custom":
                 return multi_step_spiking_vgg_custom(
                     2*args.tbin, cfg=args.cfg,
                     norm_layer=norm_layer, multi_step_neuron=ms_neuron,
-                    num_classes=2, backend="cupy"
+                    num_classes=2, backend="torch"
                 )
         else:
             vggs = {
@@ -77,5 +77,5 @@ def get_model(args):
             return vggs[version](
                 2*args.tbin, norm_layer=norm_layer,
                 multi_step_neuron=ms_neuron,
-                num_classes=2, backend="cupy"
+                num_classes=2, backend="torch"
             )
